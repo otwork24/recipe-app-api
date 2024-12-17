5c81c988f076a12e5d621d02e07a6f0dde647dc5
ot <otwork2024@gmail.com>
2024-12-17 14:14:03 -0500

    add flake8 config and fix linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """ Test the calc module."""
 
@@ -19,4 +20,4 @@ class CalcTests(SimpleTestCase):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
-        self.assertEqual(res, 5)    
+        self.assertEqual(res, 5)
